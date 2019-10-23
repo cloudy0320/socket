451ddd7668afcd22d64740b66f03c51dbc54b681
cloudy <787869622@qq.com>
2019-10-23 13:11:54 +0800

    select

--- client/main.go
+++ client/main.go
@@ -6,141 +6,157 @@ import (
 	"os"
 )
 
-var name string
+var (
+	name string
+
+	flag string
+
+	kind string
+)
 
 func main() {
-	if s := login(); s != "0" {
-		ch := make(chan int)
-		flag := ""
-		for {
-			fmt.Println("进入公共聊天室输入1，想要私聊输入2")
-			fmt.Scanln(&flag)
-			if !(flag == "1" || flag == "2") {
-				fmt.Println("只能输入1和2")
+	address, err := net.ResolveTCPAddr("tcp", ":1200")
+	checkError(err)
+	conn, err := net.DialTCP("tcp", nil, address)
+	checkError(err)
+	recvData := make([]byte, 128)
+	kind = "login"
+	for {
+		flag = ""
+		fmt.Println("1：登录，2：注册")
+		_, err = fmt.Scanln(&flag)
+		checkError(err)
+		if !(flag == "1" || flag == "2") {
+			fmt.Println("只能输入1和2")
+			continue
+		}
+		break
+	}
+	for {
+		recvData = make([]byte, 128)
+		fmt.Print("输入你的姓名:")
+		_, err = fmt.Scanln(&name)
+		checkError(err)
+		_, err = conn.Write([]byte(kind + ";" + name + ";"))
+		checkError(err)
+		n, err := conn.Read(recvData)
+		if err != nil {
+			fmt.Println("好像哪里出错了")
+			os.Exit(0)
+		}
+		if n == 0 {
+			fmt.Println("服务器和你断开了链接")
+			os.Exit(0)
+		}
+		if flag == "1" {
+			if string(recvData[0]) == "1" {
+				fmt.Println("您已成功登陆 " + name)
+				break
+			} else if string(recvData[0]) == "0" {
+				fmt.Println("没有这个名称，请先去注册")
+				continue
+			} else if string(recvData[0]) == "2" {
+				fmt.Println("这个号已经被登录了")
 				continue
 			}
-			break
+		} else if flag == "2" {
+			if string(recvData[0]) == "1" {
+				fmt.Println("您已成功注册" + name)
+				break
+			} else if string(recvData[0]) == "0" {
+				fmt.Println("这个名称已经被注册了")
+				continue
+			} else {
+				fmt.Println("服务器好像有点问题")
+				continue
+			}
 		}
-		address, err := net.ResolveTCPAddr("tcp", ":1200")
+	}
+	for {
+		flag = ""
+		fmt.Println("进入公共聊天室输入1，想要私聊输入2")
+		_, err = fmt.Scanln(&flag)
 		checkError(err)
-		conn, err := net.DialTCP("tcp", nil, address)
-		checkError(err)
-		_, err = conn.Write([]byte(name + ";" + flag + ";" + s + ";"))
-		checkError(err)
-		go recive(conn)
-		if flag == "1" {
-			go many(conn, ch)
-			<-ch
-		} else {
-			go one(conn, ch)
-			<-ch
+		if !(flag == "1" || flag == "2") {
+			fmt.Println("只能输入1和2")
+			continue
 		}
+		break
+	}
+	go receive(conn)
+	ch := make(chan int)
+	if flag == "1" {
+		go many(conn, ch)
+		<-ch
+	} else if flag == "2" {
+		go one(conn, ch)
+		<-ch
 	}
 }
 
 func checkError(err error) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		_, err := fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		checkError(err)
 		os.Exit(1)
 	}
 }
 
-func login() string {
-	address, err := net.ResolveTCPAddr("tcp", ":1201")
-	checkError(err)
-	conn, err := net.DialTCP("tcp", nil, address)
-	checkError(err)
-Again:
-	recvData := make([]byte, 128)
-	fmt.Println("1：登录，2：注册")
-	flag := ""
-	fmt.Scanln(&flag)
-	if !(flag == "1" || flag == "2") {
-		fmt.Println("只能输入1和2")
-		goto Again
-	}
-	fmt.Print("输入你的姓名")
-	fmt.Scanln(&name)
-	_, err = conn.Write([]byte(name + ";" + flag + ";"))
-	checkError(err)
-	_, err = conn.Read(recvData)
-	checkError(err)
-	if string(recvData[0]) == "1" && flag == "1" {
-		fmt.Println("成功，欢迎你 " + name)
-		conn.Close()
-		return "1"
-	} else if string(recvData[0]) == "1" && flag == "2" {
-		fmt.Println("成功，欢迎你 " + name)
-		conn.Close()
-		return "2"
-	} else if string(recvData[0]) == "2" {
-		fmt.Println("这个名称已经被注册了")
-		goto Again
-	} else if string(recvData[0]) == "3" {
-		fmt.Println("没有这个名称，请先去注册")
-		goto Again
-	} else {
-		fmt.Println("服务器好像有点问题,待会再试试")
-		conn.Close()
-		return "0"
-	}
-}
-
-func recive(conn net.Conn) {
+func receive(conn net.Conn) {
 	for {
 		recvData := make([]byte, 2048)
 		n, err := conn.Read(recvData)
 		if err != nil {
-			fmt.Println(err)
-			break
-		}
-		if n == 0 {
+			checkError(err)
 			break
 		} else {
 			recvStr := string(recvData[:n])
 			fmt.Println(recvStr)
 		}
 	}
-	os.Exit(1)
 }
 
 func one(conn net.Conn, ch chan int) {
-chat:
-	fmt.Println("请输入你要聊天的用户名")
-	toName := ""
-	fmt.Scanln(&toName)
-	_, err := conn.Write([]byte(toName + ";"))
-	checkError(err)
-	message := ""
+Again:
 	for {
-		fmt.Scanln(&message)
-		if message == "/quit" {
-			conn.Write([]byte(message))
-			goto chat
-		} else if message == "/change" {
-			conn.Write([]byte(message))
-			go many(conn, ch)
-			return
-		} else {
-			conn.Write([]byte(message))
+		kind = "one"
+		_, err := conn.Write([]byte("many" + ";" + "说" + ";" + "/list" + ";"))
+		checkError(err)
+		fmt.Println("请输入你要聊天的用户名")
+		toName := ""
+		_, err = fmt.Scanln(&toName)
+		checkError(err)
+		message := ""
+		for {
+			fmt.Scanln(&message)
+			if message == "/quit" {
+				continue Again
+			} else if message == "/change" {
+				go many(conn, ch)
+				return
+			} else {
+				_, err = conn.Write([]byte(kind + ";" + message + ";" + toName + ";"))
+				checkError(err)
+			}
 		}
 	}
 }
 
 func many(conn net.Conn, ch chan int) {
+	kind = "many"
 	for {
 		message := ""
-		fmt.Scanln(&message)
+		_, err := fmt.Scanln(&message)
+		checkError(err)
 		if message == "" {
 			fmt.Println("不能输入为空")
 			continue
 		}
 		if message == "/change" {
-			conn.Write([]byte(message))
 			go one(conn, ch)
 			return
 		} else {
-			_, err := conn.Write([]byte(name + "说" + message))
+			_, err := conn.Write([]byte(kind + ";" + name + "说" + ";" + message + ";"))
 			checkError(err)
 		}
 	}
--- service/main.go
+++ service/main.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
 	"net"
 	"os"
 	"strings"
-	"time"
-
-	"github.com/astaxie/beego/orm"
-	_ "github.com/go-sql-driver/mysql"
 )
 
 type User struct {
 	Id      int
 	Name    string
-	message *message `orm:"reverse(one)"`
+	Message *Message `orm:"reverse(one)"`
 }
 
 type user struct {
@@ -22,12 +20,12 @@ type user struct {
 	conn net.Conn
 }
 
-type message struct {
-	Id      int
-	From    string
-	To      *User `orm:"rel(one)"`
-	Message string
-	Stats   int `orm:"default(1)"`
+type Message struct {
+	Id    int
+	From  string
+	To    *User `orm:"rel(one)"`
+	Text  string
+	Stats int `orm:"default(1)"`
 }
 
 var users []user
@@ -37,206 +35,181 @@ func init() {
 
 	orm.RegisterDataBase("default", "mysql", "root:123456@/socket?charset=utf8")
 
-	orm.RegisterModel(new(User), new(message))
+	orm.RegisterModel(new(User), new(Message))
 
 	orm.RunSyncdb("default", false, true)
+
+	get()
 }
 
 func main() {
 	service := ":1200"
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	tcpAddr, err := net.ResolveTCPAddr("tcp", service)
 	checkError(err)
 	listener, err := net.ListenTCP("tcp", tcpAddr)
 	checkError(err)
-	get()
 	for {
-	A:
 		conn, err := listener.Accept()
 		if err != nil {
 			continue
 		}
-		request := make([]byte, 128)
-		_, err = conn.Read(request)
-		checkError(err)
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	defer del(conn)
+
+	readChan := make(chan []string)
+	writeLoginChan := make(chan []string)
+	writeOneChan := make(chan []string)
+	writeManyChan := make(chan []string)
+	writeRegisterChan := make(chan []string)
+	stopChan := make(chan bool)
+	var myName string
+
+	go readConn(conn, readChan, stopChan)
+	go writeLoginConn(conn, writeLoginChan, stopChan, &myName)
+	go writeRegisterConn(conn, writeRegisterChan, stopChan, &myName)
+	go writeOneConn(conn, writeOneChan, stopChan, &myName)
+	go writeManyConn(conn, writeManyChan, stopChan, &myName)
+
+	for {
+		select {
+		case array := <-readChan:
+			if array[0] == "login" {
+				fmt.Println("登录")
+				writeLoginChan <- array
+			} else if array[0] == "register" {
+				writeRegisterChan <- array
+			} else if array[0] == "one" {
+				writeOneChan <- array
+			} else if array[0] == "many" {
+				writeManyChan <- array
+			}
+		case stop := <-stopChan:
+			if stop {
+				return
+			}
+		}
+	}
+}
+
+func readConn(conn net.Conn, readChan chan<- []string, stopChan chan<- bool) {
+	for {
+		data := make([]byte, 128)
+		_, err := conn.Read(data)
+		if err != nil {
+			fmt.Println(err)
+			stopChan <- true
+		}
 		array := make([]string, 128)
-		array = strings.Split(string(request), ";")
-		if array[2] == "2" {
-			newUser := new(user)
-			newUser.conn = conn
-			newUser.Name = array[0]
-			users = append(users, *newUser)
-		} else if array[2] == "1" {
-			fmt.Println(users)
-			for i, v := range users {
-				if v.Name == array[0] && v.conn == nil {
-					users[i].conn = conn
-				} else if v.Name == array[0] && v.conn != nil {
-					_, err = conn.Write([]byte("这个账号已经被登录了！"))
+		array = strings.Split(string(data), ";")
+
+		fmt.Println("Received:", array)
+
+		readChan <- array
+	}
+}
+
+func writeLoginConn(conn net.Conn, writeChan <-chan []string, stopChan chan<- bool, name *string) {
+	for {
+		array := <-writeChan
+		*name = array[1]
+		for i, v := range users {
+			if array[1] == v.Name && v.conn == nil {
+				users[i].conn = conn
+				_, err := conn.Write([]byte("1"))
+				checkError(err)
+				check(*name, conn)
+			} else if array[1] == v.Name && v.conn != nil {
+				_, err := conn.Write([]byte("2"))
+				checkError(err)
+			}
+		}
+	}
+}
+
+func writeRegisterConn(conn net.Conn, writeChan <-chan []string, stopChan chan<- bool, name *string) {
+	o := orm.NewOrm()
+	for {
+		array := <-writeChan
+		*name = array[1]
+		u := User{Name: array[1]}
+		o.Begin()
+		if created, _, err := o.ReadOrCreate(&u, "Name"); err == nil {
+			if created {
+				o.Commit()
+				_, err = conn.Write([]byte("1"))
+				checkError(err)
+				//stopChan <- true
+			} else {
+				_, err = conn.Write([]byte("0"))
+				checkError(err)
+				continue
+			}
+		} else {
+			o.Rollback()
+			_, err = conn.Write([]byte("-1"))
+			checkError(err)
+		}
+	}
+}
+
+func writeOneConn(conn net.Conn, writeChan <-chan []string, stopChan chan<- bool, name *string) {
+	for {
+		array := <-writeChan
+		if len(array) == 4 {
+			for _, v := range users {
+				if array[2] == v.Name && v.conn != nil {
+					_, err := v.conn.Write([]byte(*name + "对你说" + array[1]))
 					checkError(err)
-					conn.Close()
-					goto A
+				} else if array[2] == v.Name && v.conn == nil {
+					o := orm.NewOrm()
+					o.Begin()
+					m := Message{Text: array[1], To: &v.User, From: *name}
+					_, err := o.Insert(&m)
+					if err != nil {
+						o.Rollback()
+					} else {
+						o.Commit()
+					}
 				}
 			}
 		} else {
 			continue
 		}
-		if array[1] == "1" {
-			check(array[0], conn)
-			go handleClient(conn, array[0])
-		} else {
-			check(array[0], conn)
-			go chat(conn, array[0])
-		}
 	}
 }
 
-func handleClient(conn net.Conn, name string) {
-	conn.SetReadDeadline(time.Now().Add(2 * time.Minute)) // set 2 minutes timeout
-	request := make([]byte, 128)                          // set maxium request length to 128B to prevent flood attack                                // close connection before exit
+func writeManyConn(conn net.Conn, writeChan <-chan []string, stopChan chan<- bool, name *string) {
 	for {
-		read_len, err := conn.Read(request)
-		//fmt.Printf("长度：%d", read_len)
-		if err != nil {
-			fmt.Println()
-			del(conn)
-			conn.Close()
-			break
-		}
-		if read_len == 0 {
-			del(conn)
-			conn.Close()
-			break // connection already closed by client
-		} else {
-			index := strings.Index(string(request), "说")
-			if string(request[index+3:index+8]) == "/list" {
-				var online []string
-				for _, v := range users {
-					if name == v.Name {
-						online = append(online, v.Name+"(你自己)")
-					} else if v.conn != nil {
-						online = append(online, v.Name+"(在线)")
-					} else {
-						online = append(online, v.Name+"(不在线)")
-					}
-				}
-				list := strings.Join(online, "\n")
-				_, err = conn.Write([]byte(list))
-				checkError(err)
-			} else if string(request[index+1:index+8]) == "/change" {
-				go chat(conn, name)
-				return
-			} else {
-				for _, v := range users {
-					if v.conn != nil {
-						_, err = v.conn.Write(request)
-						checkError(err)
-					}
+		array := <-writeChan
+		if array[2] == "/list" {
+			var online []string
+			for _, v := range users {
+				if *name == v.Name {
+					online = append(online, v.Name+"(你自己)")
+				} else if v.conn != nil {
+					online = append(online, v.Name+"(在线)")
+				} else {
+					online = append(online, v.Name+"(不在线)")
 				}
 			}
-		}
-
-		request = make([]byte, 128) // clear last read content
-	}
-}
-
-func chat(conn net.Conn, name string) {
-	conn.SetReadDeadline(time.Now().Add(2 * time.Minute)) // set 2 minutes timeout
-	flag := 1
-Again:
-	toName := make([]byte, 128)
-	request := make([]byte, 128)
-	var online []string
-	me := new(user)
-	for _, v := range users {
-		if name == v.Name {
-			online = append(online, v.Name+"(你自己)")
-			*me = v
-		} else if v.conn != nil {
-			online = append(online, v.Name+"(在线)")
+			list := strings.Join(online, "\n")
+			_, err := conn.Write([]byte(list))
+			checkError(err)
+		} else if array[2] == "/change" {
+			continue
 		} else {
-			online = append(online, v.Name+"(不在线)")
-		}
-	}
-	response := strings.Join(online, "\n")
-	conn.Write([]byte(response))
-
-	read_len, err := conn.Read(toName)
-	if err != nil {
-		fmt.Println(err)
-		conn.Close()
-		del(conn)
-		return
-	}
-	if read_len == 0 {
-		conn.Close()
-		del(conn)
-		return // connection already closed by client
-	}
-	for _, v := range users {
-		if strings.Split(string(toName), ";")[0] == v.Name && v.conn != nil {
-			for {
-				read_len, err := conn.Read(request)
-				if err != nil {
-					fmt.Println(err)
-					flag = 0
-					break
-				}
-				if read_len == 0 {
-					flag = 0
-					break // connection already closed by client
-				} else if string(request[:5]) == "/quit" {
-					goto Again
-				} else if string(request[:7]) == "/change" {
-					go handleClient(conn, name)
-					return
-				} else {
-					_, err = v.conn.Write([]byte(name + "对你说" + string(request)))
+			for _, v := range users {
+				if v.conn != nil {
+					_, err := v.conn.Write([]byte(array[1] + array[2]))
 					checkError(err)
 				}
-				request = make([]byte, 128) // clear last read content
-			}
-		} else if strings.Split(string(toName), ";")[0] == v.Name && v.conn == nil {
-			_, err := conn.Write([]byte("此人不在线，你可以发送离线消息"))
-			checkError(err)
-			for {
-				newMessage := new(message)
-				read_len, err := conn.Read(request)
-				if err != nil {
-					fmt.Println(err)
-					flag = 0
-					break
-				}
-				if read_len == 0 {
-					flag = 0
-					break // connection already closed by client
-				} else if string(request[:5]) == "/quit" {
-					goto Again
-				} else if string(request[:7]) == "/change" {
-					go handleClient(conn, name)
-					return
-				} else {
-					o := orm.NewOrm()
-					o.Begin()
-					newMessage.Message = string(request)
-					newMessage.From = me.Name
-					newMessage.To = &v.User
-					_, err := o.Insert(newMessage)
-					fmt.Println(err)
-					if err == nil {
-						o.Commit()
-					}
-				}
-				request = make([]byte, 128) // clear last read content
 			}
 		}
-		if flag == 0 {
-			break
-		}
-	}
-	if flag == 1 {
-		_, err := conn.Write([]byte("没有这个人，请重新输入\n"))
-		checkError(err)
-		goto Again
 	}
 }
 
@@ -278,11 +251,11 @@ func check(name string, conn net.Conn) {
 	if err == orm.ErrNoRows {
 		fmt.Println("查询不到")
 	}
-	var messages []message
+	var messages []Message
 	n, err := o.QueryTable("message").Filter("To", userTemp.Id).Filter("stats", 0).All(&messages)
 	if err == nil && n != 0 {
 		for _, v := range messages {
-			response = append(response, v.From+"对你说"+v.Message)
+			response = append(response, v.From+"对你说"+v.Text)
 			v.Stats = 1
 			o.Update(&v)
 		}
